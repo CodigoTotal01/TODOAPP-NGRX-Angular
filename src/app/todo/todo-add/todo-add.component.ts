ca0b5fec61834493feda7a9ed45f2720a9b4be65
CodigoTotal01 <palaciostarrillochristian@gmail.com>
2023-09-07 21:55:48 -0500

    Inicializando proyecto

---
+++ src/app/todo/todo-add/todo-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-add',
+  templateUrl: './todo-add.component.html',
+  styles: [
+  ]
+})
+export class TodoAddComponent {
+
+}
